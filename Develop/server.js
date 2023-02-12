38e23aa07e46c2c79369da5c5e82e8dc6734e416
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-11 19:21:28 -0500

    updated server code

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 8080;
+
+app.use(express.json ());
+app.use(express.urlencoded({extented: true}));
+app.use(express.static("public"));
+
+require('./Routes/ApiRoutes') (app);
+require ('./Routes/HtmlRoutes') (app);
+
+app.listen(PORT, () => {
+    console.log(`app server on PORT: ${PORT}`);
+  });
