9cf29535b3b947cc2021add50023a2216190f4cc
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-18 20:42:56 -0500

    update changes to add note

--- Develop/Routes/ApiRoutes.js
+++ Develop/Routes/ApiRoutes.js
@@ -1,22 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
+const db = require('../db/db.json');
+
 module.exports = function(app){
     app.get("/api/notes", (req, res) => {
-	    const db = require('../db/db.json');
-		return res.send(db)
+		// Read data in realtime from the file
+		const data = fs.readFileSync('./db/db.json', 'UTF-8');
+		// Send data back to server
+		res.send(data)
       });
 
 	app.post('/api/notes', (req, res) => {
-		// Implement create note logic here
+		// Read the JSON file as UTF8 to get current notes
+		const savedNotes = fs.readFileSync('./db/db.json', 'UTF-8');
+		// Parse the data to convert from string to JSON
+		const data = JSON.parse(savedNotes);
+		// Add the note to the data
+		data.push(req.body)
+		// Write the new data to the file to override
+		fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(data));
+		// Send updated data back to server
+		res.send(JSON.stringify(data))
 	})
 
-	app.delete('/api/notes', (req, res) => {
-		// Implement the delete note logic here
-	})
+
 }
-
-
 
     
     
