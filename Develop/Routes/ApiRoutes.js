47a707ef1d3cf6396bc6bb104972285dec0d6b59
Mrose332 <116510026+Mrose332@users.noreply.github.com>
2023-02-18 20:47:30 -0500

    commit new changes

--- Develop/Routes/ApiRoutes.js
+++ Develop/Routes/ApiRoutes.js
@@ -5,14 +5,14 @@ const db = require('../db/db.json');
 
 module.exports = function(app){
     app.get("/api/notes", (req, res) => {
-		// Read data in realtime from the file
+		
 		const data = fs.readFileSync('./db/db.json', 'UTF-8');
-		// Send data back to server
+	
 		res.send(data)
       });
 
 	app.post('/api/notes', (req, res) => {
-		// Read the JSON file as UTF8 to get current notes
+		
 		const savedNotes = fs.readFileSync('./db/db.json', 'UTF-8');
 	
 		const data = JSON.parse(savedNotes);
